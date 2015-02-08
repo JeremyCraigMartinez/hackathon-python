c79e14d7579cc7e2d3c527a8f9caf7425b7cbddf
JeremyCraigMartinez <jeremymartinez11@gmail.com>
2015-02-08 00:00:14 -0800

    kill scripts and unlimited (not 10000)

--- main.py
+++ main.py
@@ -11,13 +11,7 @@ import socket
 from csvfile import *
 
 if __name__ == "__main__":
-	count = 0
-	hits = []
 	for contents in parse(sys.argv[1]):
-		count = count + 1
-		if count > 10000:
-			print(hits)
-			sys.exit()
 		list_of_everything = []
 		if len(sys.argv) > 1:
 			for each in contents:
@@ -27,12 +21,11 @@ if __name__ == "__main__":
 					domain = ip_lookup(each_ip)
 					button = button_lookup(domain)
 					if domain != None:
-						hits = hits + [{'domain':domain,'button':button}]
 						list_of_everything = list_of_everything + [{'domain':domain,'button':button}]
 		else:
 			print("Enter file...")
 
-		url = 'http://104.236.169.12:5024/hello'
+		url = 'http://104.236.169.12:5024/'
 		for each in list_of_everything:
 			params = json.dumps(each).encode('utf8')
 
